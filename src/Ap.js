1106347c5b56d455b24eda678f1a3b20509a2887
Fabio Galante Mans <fabiogalantemans@gmail.com>
2022-01-01 17:32:54 -0300

    Card Component & Conditional Styles

--- src/Ap.js
+++ src/Ap.js
@@ -1,20 +1,24 @@
+import { useState } from 'react';
 import React from 'react';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 // function App() {
 //     return React.createElement('div', { className: 'container'}, React.createElement('h1', {}, 'My App'))
 // }
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,25 +1,25 @@
-//rfce
+//_rfce
 
-import { useState } from 'react';
+import Card from './shared/Card'
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState('This is an exemple of a feedback item');
+function FeedbackItem({ item }) {
+  //const [rating, setRating] = useState(7);
+  //const [text, setText] = useState('This is an exemple of a feedback item');
 
-    const handleClick = () => {
-        //setRating(10)
-        setRating((prev) => {
-            console.log(prev);
-            return prev + 1 
-        })
-    }
+  // const handleClick = () => {
+  //     //setRating(10)
+  //     setRating((prev) => {
+  //         console.log(prev);
+  //         return prev + 1
+  //     })
+  // }
 
-    return (
-    <div className='card'>
-      <div className='num-display'>{rating}</div>
-      <div className='text-display'> {text} </div>
-      <button onClick={handleClick}>Click</button>
-    </div>
+  return (
+    <Card reverse={false}>
+      <div className='num-display'>{item.rating}</div>
+      <div className='text-display'> {item.text} </div>
+      {/* <button onClick={handleClick}>Click</button> */}
+    </Card>
   );
 }
 
